5a6174c125a7590cf8d9f331625de0fc7e23f23a
AndrePruden <andrepruden0@gmail.com>
2024-05-07 10:11:59 -0400

    Encuentra un proyecto cando el mismo existe en una lista de 1 proyecto

--- src/buscar.test.js
+++ src/buscar.test.js
@@ -5,8 +5,21 @@ describe("Buscar Proyectos", () => {
     let proyectos = [];
     expect(buscarProyecto("ejercicio1", proyectos)).toEqual("");
   });
+
+  it("Encuentra un proyecto cuando el mismo existe en una lista de 1 proyecto", () => {
+    let proyectos = [];
+    proyectos.push("miUnicoProyecto");
+    expect(buscarProyecto("miUnicoProyecto", proyectos)).toEqual("miUnicoProyecto");
+  });
+
+
 });
 
-function buscarProyecto(){
-  return "";
+function buscarProyecto(nombreProyecto, proyectos){
+  if(proyectos.length === 0){
+    return "";
+  }
+  if(proyectos[0] === nombreProyecto){
+    return nombreProyecto;
+  }
 }
